f1d2d8f9ab2e1b67bc950e5f4f8540070ca55fd8
Sergei <sergeitrigubovfleps@gmail.com>
2020-11-26 01:02:54 +0300

    fix unique number-user_id comb

--- db/migrate/20201125113827_create_contacts.rb
+++ db/migrate/20201125113827_create_contacts.rb
@@ -2,8 +2,9 @@ class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.string :number, unique: true
+      t.string :number
       t.references :user, null: false, foreign_key: true
+      t.index [:number, :user_id], unique: true
 
       t.timestamps
     end
